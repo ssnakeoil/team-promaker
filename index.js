31e6d4176302954adb150885428f4c0c183ab4ec
Stefi <114958335+ssnakeoil@users.noreply.github.com>
2023-03-29 20:25:51 -0400

    adding manager dialogue

--- index.js
+++ index.js
@@ -4,3 +4,45 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+
+const teamMembers = [];
+const employeeIds = [];
+
+console.log("Welcome to Team Pro(file)Maker!");
+
+function dialogue() {
+    function createManager() {
+        console.log('Build your team!');
+        inquirer
+          .prompt([
+            {
+                type: 'input',
+                name: 'managerName',
+                message: 'Please enter the manager\'s name:',
+            },
+            {
+                type: 'input',
+                name: 'managerId',
+                message: 'Please enter the manager\'s ID:',
+            },
+            {
+                type: 'input',
+                name: 'managerEmail',
+                message: 'Please enter the manager\'s email:',
+            },
+            {
+                type: 'input',
+                name: 'managerOfficeNumber',
+                message: 'Please enter the manager\'s office number:',
+            },
+            
+        ])
+        .then(answers => {
+            const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+            teamMembers.push(manager);
+            employeeIds.push(answers.managerId);
+            createTeam();
+        });
+    }
+
+    function createTeam() {}}
