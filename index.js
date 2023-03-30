ff6ff3a7eda75517f6d9920e944870a96935cb32
Stefi <114958335+ssnakeoil@users.noreply.github.com>
2023-03-29 21:27:33 -0400

    establishing employee class and exporting it

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "output.html");
+
+const render = require("./src/page-template.js");
+
 const teamMembers = [];
 const employeeIds = [];
 
